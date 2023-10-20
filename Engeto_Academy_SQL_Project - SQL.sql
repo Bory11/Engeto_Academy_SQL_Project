fdb40b876622a682084b4df256e1b25e6bfe721f
Marek Borč <marekborc@seznam.cz>
2023-10-20 14:52:18 +0200

    Finished the tables. Began looking for answers to research questions.

--- Engeto_Academy_SQL_Project - SQL.sql
+++ Engeto_Academy_SQL_Project - SQL.sql
@@ -16,8 +16,8 @@ SELECT *
 FROM czechia_payroll AS cp 
 ORDER BY payroll_year ASC, payroll_quarter ASC;
 
--- Ceny v czechia_payroll od prvního kvartálu 2000 do druhého kvartálu 2021 --
--- Prolíná se období leden 2006 - prosinec 2018 --
+/* Ceny v czechia_payroll od prvního kvartálu 2000 do druhého kvartálu 2021 ->
+ Prolíná se období leden 2006 - prosinec 2018  */
 
 -- Alternativně ???--
 SELECT 
@@ -35,23 +35,28 @@ GROUP BY date_from ;
  */
 
 CREATE OR REPLACE TABLE t_marek_borč_project_SQL_primary_final
-AS SELECT
-	cp.value,
-	cpu.name,
-	cpib.name AS Odvětví,
-	cp.payroll_year
-FROM czechia_payroll AS cp
-JOIN czechia_payroll_value_type AS cpvt ON cp.value_type_code = cpvt.code
-JOIN czechia_payroll_unit AS cpu ON cp.unit_code = cpu.code
-JOIN czechia_payroll_industry_branch AS cpib ON cp.industry_branch_code = cpib.code
-WHERE cpvt.code = 5958 AND cp.payroll_year >= 2006 AND cp.payroll_year <=2018
+SELECT
+	cpc.name AS product,
+	cp.value AS price,
+	DATE_FORMAT(cp.date_from, '%Y-%m-%d') AS price_measured_from,
+    DATE_FORMAT(cp.date_to, '%Y-%m-%d') AS price_measured_to,
+	cpib.name AS industry,
+	cpay.value AS payroll,
+	cpay.payroll_year
+FROM czechia_price AS cp 
+JOIN czechia_payroll AS cpay 
+	ON cpay.payroll_year = YEAR(cp.date_from)
+	AND cpay.payroll_year BETWEEN 2006 AND 2018
+	AND cpay.value_type_code = 5958
+JOIN czechia_price_category AS cpc 
+	ON cp.category_code = cpc.code 
+LEFT JOIN czechia_payroll_industry_branch AS cpib 
+	ON cpay.industry_branch_code = cpib.code 
 ;
 
-SELECT
-	round(avg(value), 0),
-	payroll_year
-FROM t_marek_borč_project_sql_primary_final AS tmbpspf 
-GROUP BY payroll_year;
+/*
+ * CREATE TABLE trvá deset minut bez ohledu sekvenci. Zřejmě normální při množství dat?
+ */
 
 -- Pomocné dotazy --
 SELECT * FROM czechia_payroll AS cp 
@@ -59,7 +64,7 @@ WHERE value_type_code = 5958 AND value IS NULL;
 
 /* Dodatečný materiál připravte i tabulku s HDP, GINI koeficientem 
  * a populací dalších evropských států ve stejném období, jako primární přehled pro ČR.
- * a) Economies obsahuje kombinaci států a regionů - nutná shoda jmen mezi countries a economies
+ * Economies obsahuje kombinaci států a regionů - nutná shoda jmen mezi countries a economies
  */
 
 CREATE OR REPLACE TABLE t_marek_borč_project_SQL_secondary_final
@@ -71,9 +76,6 @@ AS SELECT
 	e.gini	
 FROM countries AS c
 LEFT JOIN economies AS e ON c.country = e.country
-WHERE YEAR >= 2006 AND YEAR <=2018
+WHERE YEAR >= 2006 AND YEAR <=2018 AND c.continent = 'Europe'
 ;
 
-
-	
-	
