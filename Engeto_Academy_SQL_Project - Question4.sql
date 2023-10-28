8dfcf36d1fc3affded0e0910055a7564980115d7
Marek Borč <marekborc@seznam.cz>
2023-10-28 22:14:45 +0200

    Term changes in Q4.

--- Engeto_Academy_SQL_Project - Question4.sql
+++ Engeto_Academy_SQL_Project - Question4.sql
@@ -11,12 +11,12 @@ SELECT
 	vas.payroll_year,
 	round(avg(vas.avg_wage), 2) AS avg_wage,
 	round(lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year), 2) AS avg_wage_prev_year,
-	round(avg(vas.avg_wage) - lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year), 2) AS YoY_wage_difference,
-	round((avg(vas.avg_wage) - lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year)) / (lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year))*100, 2) AS YoY_wage_difference_percentual,
+	round(avg(vas.avg_wage) - lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year), 2) AS YoY_wage_growth,
+	round((avg(vas.avg_wage) - lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year)) / (lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year))*100, 2) AS YoY_wage_growth_percentual,
 	round(avg(vap.avg_price), 2) AS avg_price,
 	round(lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year), 2) AS avg_price_prev_year,
-	round(avg(vap.avg_price), 2) - round(lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year), 2) AS YoY_price_difference,
-	round((avg(vap.avg_price) - lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year)) / (lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year))*100, 2) AS YoY_price_difference_percentual,
+	round(avg(vap.avg_price), 2) - round(lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year), 2) AS YoY_price_growth,
+	round((avg(vap.avg_price) - lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year)) / (lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year))*100, 2) AS YoY_price_growth_percentual,
 	round((avg(vap.avg_price) - lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year)) / (lag(avg(vap.avg_price)) OVER (ORDER BY vas.payroll_year))*100, 2) - round((avg(vas.avg_wage) - lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year)) / (lag(avg(vas.avg_wage)) OVER (ORDER BY vas.payroll_year))*100, 2) AS YoY_price_wage_difference_perc
 FROM v_average_sector_wage_by_year AS vas
 JOIN v_average_product_price_by_year AS vap 
@@ -45,12 +45,12 @@ SELECT
 	payroll_year,
 	avg_wage,
 	avg_wage_prev_year,
-	avg_wage - avg_wage_prev_year AS YoY_wage_difference,
-	round((avg_wage - avg_wage_prev_year) / avg_wage_prev_year*100, 2) AS YoY_wage_difference_percentual,
+	avg_wage - avg_wage_prev_year AS YoY_wage_growth,
+	round((avg_wage - avg_wage_prev_year) / avg_wage_prev_year*100, 2) AS YoY_wage_growth_percentual,
 	avg_price,
 	avg_price_prev_year,
-	avg_price - avg_price_prev_year AS YoY_price_difference,
-	round((avg_price - avg_price_prev_year) / avg_price_prev_year*100, 2) AS YoY_price_difference_percentual,
+	avg_price - avg_price_prev_year AS YoY_price_growth,
+	round((avg_price - avg_price_prev_year) / avg_price_prev_year*100, 2) AS YoY_price_growth_percentual,
 	round((avg_price - avg_price_prev_year) / avg_price_prev_year*100,2) - round((avg_wage - avg_wage_prev_year) / avg_wage_prev_year*100, 2) AS YoY_price_wage_difference_perc
 FROM rolling_wages_and_price;
 	
